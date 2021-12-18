a5d7ea1aedb7c5c8265341edfa67c01c103928cc
Rafael <ikbenrafaelbieze2001@gmail.com>
2021-12-18 22:38:26 +0100

    Forgot in last commit

--- scripts/libs/execute.ts
+++ scripts/libs/execute.ts
@@ -1,19 +1,10 @@
 import { spawn } from "child_process";
 
 export function exec(command: string, args: readonly string[] = [], callback?: (code: number | null) => void) {
-    var spRet = spawn(command, args, {
+    const spRet = spawn(command, args, {
         windowsHide: true,
+        stdio: "inherit"
     });
-    spRet.stdout.setEncoding("utf8");
-    spRet.stdout.on('data', function (data) {
-        process.stdout.write(data);
-    });
-
-    spRet.stderr.setEncoding('utf8');
-    spRet.stderr.on('data', function (data) {
-        process.stderr.write(data);
-    });
-
     spRet.on('close', (code) => {
         if (callback) callback(code);
     })
